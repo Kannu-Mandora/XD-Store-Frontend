2ddb35587a22f0da4a7db25c9d2711341e95f53f
“Kannu-Mandora” <mandorakannu@gmail.com>
2022-12-18 12:53:37 +0530

    Add Sale-Banner.png In Main.js

--- src/Components/mobile/Main.js
+++ src/Components/mobile/Main.js
@@ -12,8 +12,11 @@ const Main = () => {
       </div>
       <Categorybar />
       <Sale/>
+      <div className="flex flex-col">
+      <img src="https://ik.imagekit.io/egkxyv8la/sale-banner_o54ZFsooY8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1671348120697" alt="" className="h-72" />
       <div className="flex justify-start items-center cards overflow-x-scroll my-4">
       <Cards/>
+      </div>
       </div>
     </>
   );
